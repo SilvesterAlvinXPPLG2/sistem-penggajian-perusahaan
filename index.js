2576bdaac310771d64c0c90bf95b49494e8317ed
Akv1gn <oktavianoalvin1@gmail.com>
2025-02-19 20:01:16 +0700

    sistem penggajian

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ let kehadiranfull = 200000;
 let gajian = 5000000;
 let bonus = 500000;
 let denda = 3000000;
-let pelanggaran = 2;
 
-if (pegawaimasuk > 31) {
+if (pegawaimasuk >= 31) {
     console.log(`pegawai mendapatkan gaji sebesar ${gajian + bonus + kehadiranfull}`);
-} else if (pegawaimasuk <= 20) {
+} else if (pegawaimasuk >= 20) {
     console.log(`pegawai mendapatkan gaji sebesar ${gajian + bonus}`);
-} else if (pegawaimasuk >= pelanggaran) {
+} else if (pegawaimasuk <= 1) {
     console.log(`pegawai mendapatkan gaji sebesar ${gajian + bonus + kehadiranfull - denda}`);
 }
